3dfefcd7b9507fcfd1f0a8b5a459e00fad9531b0
Greenbirch2007 <291109028@qq.com>
2019-07-22 16:48:39 +0800

    c++开始

---
+++ T/t.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    double n1=0.0;
+    double n2=0.0;
+    char operation=0;
+
+    printf("\nEnter the calculation\n");
+    scanf("%lf%c%lf"&n1,&operation,&n2);
+
+    return 0;
+}
